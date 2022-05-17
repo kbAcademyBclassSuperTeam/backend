a9a9f0515b55a8fb17a3e100ef2ca7185b320be2
sonhyeono <oktlrp7624583@gmail.com>
2022-05-18 07:02:03 +0900

    Up

--- post/urls.py
+++ post/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.post , name='post'),
     path('search/', views.search_table, name="search_table"),
     path('blob/', views.blob_table, name="blob_table"),
-    path('createSound', views.createSound, name="create_sound"),
+    path('createSound/', views.createSound, name="create_sound"),
     
 
 ]
