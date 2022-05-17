869b2f8c19383bac245722103bf255f2724e2e22
sonhyeono <oktlrp7624583@gmail.com>
2022-05-18 06:46:59 +0900

    [Update]

--- post/views.py
+++ post/views.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Post
 import joblib
+import wave
+import requests
+import json
+import webbrowser
 
 def post(request):
     posts = Post.objects.all()
@@ -27,8 +31,59 @@ def search_table(request):
     return JsonResponse(context)
     # return render(request,'home.html',context)
 
+# def blob_table(request):
+    
+#     blob = request.FILES['file']
+    
+#     print(blob)
+#     print("33333333333333ㄱ미ㅓㄹㅇ뮈ㅏ루비잘")
+#     nchannels = 1
+#     sampwidth = 1
+#     framerate = 8000
+#     nframes = 1
+#     name = 'audio.wav'
+#     audio = wave.open(name, 'wb')
+#     audio.setnchannels(nchannels)
+#     audio.setsampwidth(sampwidth)
+#     audio.setframerate(framerate)
+#     audio.setnframes(nframes)
+#     blob = open("audio.wav").read() # such as `blob.read()`
+#     audio.writeframes(blob.decode())
+    
+#     print(audio)
+        
+    
 def blob_table(request):
     blob_key = request.GET.get('blob_url')
+    print(blob_key)
     context = {'blob_url' : blob_key}
     
+    urL=blob_key
+    chrome_path="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path))
+    webbrowser.get('chrome').open_new_tab(urL)
+    
+    
+    
+    # def download_file(url):
+    #     # local_filename = url.split('/')[-1]
+    #     # NOTE the stream=True parameter below
+    #     with requests.get(url, stream=True) as r:
+    #         r.raise_for_status()
+    #         with open(url, 'wb') as f:
+    #             for chunk in r.iter_content(chunk_size=8192): 
+    #                 # If you have chunk encoded response uncomment if
+    #                 # and set chunk_size parameter to None.
+    #                 #if chunk: 
+    #                 f.write(chunk)
+    #     return url
+    
+    # download_file(blob_key)
+    
+    # json_txt = requests.get(blob_key).text
+    # document = json.loads(json_txt)
+
+    # print("ㅇㅇㅇㅇㅇㅇㅇㅇ")
+    # print(document)
     return JsonResponse(context)
+
