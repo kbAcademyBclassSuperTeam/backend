6c83c2396e7a437161f6d524ac4b80360f5d17aa
Jiwon Hwang <ediblepotato@gmail.com>
2022-05-17 18:58:21 +0900

    chore: sound save

--- post/views.py
+++ post/views.py
@@ -1,7 +1,12 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, Sound
+from django.conf import settings
 import joblib
+import json
+from binascii import a2b_base64
+import base64
+
 
 def post(request):
     posts = Post.objects.all()
@@ -18,8 +23,6 @@ def model_predict(texts):
 
     return y_pred[0]
     
-    
-
 def search_table(request):
     search_key = request.GET.get('search_key') 
     context = {'search_key': str(model_predict(search_key))} 
@@ -32,3 +35,23 @@ def blob_table(request):
     context = {'blob_url' : blob_key}
     
     return JsonResponse(context)
+
+def createSound(request):
+
+    data = request.body
+    url = json.loads(data).get('url')
+
+    binary_data = decode_base64_url(url)
+    with open('./test.wav', 'wb') as f:
+        f.write(binary_data)
+        f.close()
+    
+    return redirect('http://127.0.0.1:8000')
+
+def decode_base64_url(url):
+    assert url.startswith('data:audio/')
+    assert ';base64,' in url
+    schema, payload = url.split(',', 1)
+    # Maybe parse the schema if you want to know the image's type
+    # type = schema.split('/', 1)[-1].split(';', 1)[0]
+    return base64.urlsafe_b64decode(payload)
