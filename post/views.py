185a7e97a29e52a8fffc20e82cbd7edbd7f4f743
Byeongzi2 <ppoppy8080@naver.com>
2022-05-18 15:20:33 +0900

    add emotion classification VGG16 Model

--- post/views.py
+++ post/views.py
@@ -9,9 +9,12 @@ import base64
 import pandas as pd
 import tensorflow as tf
 import librosa
+import librosa.display
+import matplotlib.pyplot as plt
 import numpy as np
 import itertools
-
+import cv2
+import os
 
 def post(request):
     posts = Post.objects.all()
@@ -125,9 +128,35 @@ def scam_model():
     return pred_now[0]
         
         
+def emotionclassification() :
+    targetlist = ['Angry','Disgust','Fearful','Happy','Neutral','Sad','Surprise','Emergency']
+    model = tf.keras.models.load_model('post\model_folder\MelSpectrogramVGG16(2).h5')
+    wav_path = './test.wav'
+    save_path = './imgtest.png'
+    y, sr = librosa.load(wav_path, sr=16000)
+
+    input_nfft = int(round(sr*0.032))    
+    input_stride = int(round(sr*0.010))
     
+    S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nfft, hop_length=input_stride)
     
+    print("Wav length: {}, Mel_S shape:{}".format(len(y)/sr,np.shape(S)))
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel-Spectrogram')
+    plt.tight_layout()
+    plt.savefig(save_path, bbox_inches='tight', pad_inches=0, dpi=300, frameon='false')
     
+    img = cv2.imread(save_path)
+    img = cv2.resize(img,(224,224))     # resize image to match model's expected sizing
+    img = img.reshape(1,224,224,3) # return the image with shaping that TF wants.
+    prediction = model.predict(img)
+
+    target_num = np.argmax(prediction[0])
+    print(targetlist[target_num],',',target_num)
+    
+    return [target_num]
 
 def createSound(request):
 
@@ -135,17 +164,18 @@ def createSound(request):
     url = json.loads(data).get('url')
 
     binary_data = decode_base64_url(url)
-    with open(settings.MEDIA_ROOT + '/test.wav', 'wb') as f:
+    with open('./test.wav', 'wb') as f:
         f.write(binary_data)
         f.close()
         
     scam_score = scam_model()
-    print(scam_score[0])
+    vgg_score = emotionclassification()
+    print(scam_score[0], vgg_score[0])
     datas = {
         'scam': scam_score[0].tolist(),
+        'vgg' : vgg_score[0].tolist(),
     }
     return JsonResponse(datas)
-    
     
     
     
